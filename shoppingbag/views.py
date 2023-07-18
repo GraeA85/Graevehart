18703d6f3abb16dc684f67e978d76b8ae331b49b
GraeA85 <graemearthur36@gmail.com>
2023-07-18 11:06:17 +0000

    changes to shopping bag remove item function

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
+
 
 def view_shoppingbag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'shoppingbag/shoppingbag.html')
 
+
 def add_to_shoppingbag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    shoppingbag = request.session.get('shoppingbag', {})
-
+    
     if item_id in list(shoppingbag.keys()):
         shoppingbag[item_id] += quantity
     else:
@@ -21,3 +22,34 @@ def add_to_shoppingbag(request, item_id):
 
     request.session['shoppingbag'] = shoppingbag
     return redirect(redirect_url)
+    
+
+def adjust_shoppingbag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    if quantity > 0:
+        shoppingbag[item_id] = quantity
+    else:
+        shoppingbag.pop(item_id)
+
+    request.session['shoppingbag'] = shoppingbag
+    return redirect(reverse('view_shoppingbag'))
+
+
+def remove_from_shoppingbag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        shoppingbag = request.session.get('shoppingbag', {})
+        shoppingbag.pop(item_id)
+
+        request.session['shoppingbag'] = shoppingbag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
