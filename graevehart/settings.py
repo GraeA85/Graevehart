76ae6ad9df34e07ea63672a1e65ec127ec067024
GraeA85 <graemearthur36@gmail.com>
2023-07-20 21:17:17 +0000

    change to email settings

--- graevehart/settings.py
+++ graevehart/settings.py
@@ -211,6 +211,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST')
