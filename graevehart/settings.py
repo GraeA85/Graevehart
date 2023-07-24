fadee296f5e9d28dfe483263545179c0639eeec0
GraeA85 <graemearthur36@gmail.com>
2023-07-24 18:28:21 +0000

    chage to email setting

--- graevehart/settings.py
+++ graevehart/settings.py
@@ -205,7 +205,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'graevehart@example.com'
+    DEFAULT_FROM_EMAIL = 'graevehart@graevehart.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
